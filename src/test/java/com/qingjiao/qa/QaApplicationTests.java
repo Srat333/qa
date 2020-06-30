ae286060c55ed42e219e4f74063ba77a8c7b9b28
Ruofan Wang <1009924028@qq.com>
2020-06-30 11:00:56 -0500

    init

---
+++ src/test/java/com/qingjiao/qa/QaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qingjiao.qa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
